027d42e85d0c371ea20a604678440dcec2652135
WebEngChild <WebEngChild@example.com>
2023-08-04 06:37:49 +0900

    feat: Terraform設定

---
+++ .aws/dynamodb.tf
@@ -0,0 +1,16 @@
+resource "aws_dynamodb_table" "users" {
+  name           = "users"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "user_id"
+  range_key      = "timestamp"
+
+  attribute {
+    name = "user_id"
+    type = "S"
+  }
+
+  attribute {
+    name = "timestamp"
+    type = "S"
+  }
+}
---
+++ .aws/lamda.tf
@@ -0,0 +1,53 @@
+resource "aws_lambda_function" "lambda" {
+  function_name    = "my_lambda"
+  handler          = "index.handler"
+  runtime          = "nodejs16.x"
+  role             = aws_iam_role.lambda_role.arn
+  filename         = "function.zip"
+}
+
+resource "aws_lambda_event_source_mapping" "event_source_mapping" {
+  event_source_arn  = aws_sqs_queue.queue.arn
+  function_name     = aws_lambda_function.lambda.arn
+  enabled           = true
+  batch_size        = 5
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_policy" {
+  name   = "lambda_policy"
+  role   = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ses:SendEmail",
+          "sqs:DeleteMessage",
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+        ],
+        Resource = "*"
+      },
+    ]
+  })
+}
