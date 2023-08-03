6196bd012aa286509b3ee5fb9161919521389798
WebEngChild <WebEngChild@example.com>
2023-08-04 06:50:18 +0900

    feat: Terraform設定

--- .aws/dynamodb.tf
+++ .aws/dynamodb.tf
@@ -1,8 +1,8 @@
 resource "aws_dynamodb_table" "users" {
-  name           = "users"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "user_id"
-  range_key      = "timestamp"
+  name         = "users"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "user_id"
+  range_key    = "timestamp"
 
   attribute {
     name = "user_id"
--- .aws/lamda.tf
+++ .aws/lamda.tf
@@ -1,16 +1,16 @@
 resource "aws_lambda_function" "lambda" {
-  function_name    = "my_lambda"
-  handler          = "index.handler"
-  runtime          = "nodejs16.x"
-  role             = aws_iam_role.lambda_role.arn
-  filename         = "function.zip"
+  function_name = "my_lambda"
+  handler       = "index.handler"
+  runtime       = "nodejs16.x"
+  role          = aws_iam_role.lambda_role.arn
+  filename      = "function.zip"
 }
 
 resource "aws_lambda_event_source_mapping" "event_source_mapping" {
-  event_source_arn  = aws_sqs_queue.queue.arn
-  function_name     = aws_lambda_function.lambda.arn
-  enabled           = true
-  batch_size        = 5
+  event_source_arn = aws_sqs_queue.queue.arn
+  function_name    = aws_lambda_function.lambda.arn
+  enabled          = true
+  batch_size       = 5
 }
 
 resource "aws_iam_role" "lambda_role" {
@@ -31,8 +31,8 @@ resource "aws_iam_role" "lambda_role" {
 }
 
 resource "aws_iam_role_policy" "lambda_policy" {
-  name   = "lambda_policy"
-  role   = aws_iam_role.lambda_role.id
+  name = "lambda_policy"
+  role = aws_iam_role.lambda_role.id
 
   policy = jsonencode({
     Version = "2012-10-17",
